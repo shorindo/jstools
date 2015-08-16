589b8c9abb5f9cdde488000adf82ffa43b8fd3f3
Kazuyuki Matsuda <kazm@shorindo.com>
2015-08-16 17:36:54 +0900

    trace available on w2ui.

--- src/com/shorindo/jstools/template/analyze.js
+++ src/com/shorindo/jstools/template/analyze.js
@@ -1,32 +1,86 @@
 $(function() {
     var currFunc;
-    var grid = $().w2grid({
-        name: 'profile',
-        show: {
-            toolbar: false,
-            footer: true
-        },
-        columns: [
-            { field: 'name', caption: 'name', size: '100%', sortable: true },
-            { field: 'count', caption: 'count', size: '80px', sortable: true, render:'int' },
-            { field: 'elapsed', caption: 'elapsed', size: '150px', sortable: true, render:'float:3' },
-            { field: 'effective', caption: 'effective', size: '150px', sortable: true, render:'float:3' }
-        ],
-        onClick: function(evt) {
-            console.log(evt);
-            if (evt.column === 0 && evt.recid != 0) {
-                currFunc = evt.recid;
-                w2ui.layout.panels[0].tabs.enable('tab-source');
-                w2ui.layout.panels[0].tabs.click('tab-source');
+    var summary = [];
+    
+    function initProfile() {
+        var summary = {};
+
+        function calc(data) {
+            data.effective = 0;
+            var sum = summary[data.id];
+            if (data.id in summary) {
+                sum.count++;
+                sum.elapsed += data.elapsed;
+            } else {
+                sum = summary[data.id] = {
+                        id: data.id,
+                        fileName: functionMap.files[functionMap.functions[data.id].fileId],
+                        name: functionMap.functions[data.id].name,
+                        count: 1,
+                        elapsed: data.elapsed,
+                        effective: data.elapsed
+                    };
+            }
+            for (var i = 0; i < data.children.length; i++) {
+                calc(data.children[i]);
+            }
+            for (var i = 0; i < data.children.length; i++) {
+                sum.effective -= data.children[i].effective;
             }
         }
-    });
+
+        calc(root);
+        var records = [];
+        for (var key in summary) {
+            var rec = summary[key];
+            records.push({
+               recid: rec.id,
+               name: esc(rec.name),
+               count: rec.count,
+               elapsed: rec.elapsed,
+               effective: rec.effective
+            });
+        }
+        records.sort(function(a, b) {
+            return a.effective < b.effective ? 1 :
+                (a.effective > b.effective ? -1 : 0);
+        });
+
+        var grid = $('#content-profile').w2grid({
+            name: 'profile',
+            show: {
+                toolbar: false,
+                footer: true
+            },
+            columns: [
+                { field: 'name', caption: 'name', size: '100%', sortable: true },
+                { field: 'count', caption: 'count', size: '80px', sortable: true, render:'int' },
+                { field: 'elapsed', caption: 'elapsed', size: '150px', sortable: true, render:'float:3' },
+                { field: 'effective', caption: 'effective', size: '150px', sortable: true, render:'float:3' }
+            ],
+            onClick: function(evt) {
+                if (evt.column === 0 && evt.recid != 0) {
+                    currFunc = evt.recid;
+                    w2ui.layout.panels[0].tabs.enable('tab-source');
+                    w2ui.layout.panels[0].tabs.click('tab-source');
+                }
+            },
+            sortData: [
+                { "field": "effective", "direction": "DESC" }
+            ],
+            records: records
+        });
+    }
 
     $('#layout').w2layout({
         name    : 'layout',
         panels  : [
             {
                 type: 'main', 
+                content:
+                    '<div id="content-profile"></div>' +
+                    '<div id="content-trace"></div>' +
+                    '<div id="content-source"></div>',
                 tabs: {
                     active: 'tab-profile',
                     tabs: [
@@ -37,30 +91,72 @@ $(function() {
                     onClick: function (id, data) {
                         switch(id) {
                         case 'tab-profile':
-                            w2ui.layout.content('main', grid);
+                            $('#content-profile').css('display', 'block');
+                            $('#content-trace').css('display', 'none');
+                            $('#content-source').css('display', 'none');
                             break;
                         case 'tab-trace':
-                            w2ui.layout.content('main', '');
+                            var o = $(w2ui.layout.el('main'));
+                            $('#content-profile').css('display', 'none');
+                            $('#content-trace').css('display', 'block');
+                            $('#content-source').css('display', 'none');
                             break;
                         case 'tab-source':
+                            $('#content-profile').css('display', 'none');
+                            $('#content-trace').css('display', 'none');
+                            $('#content-source').css('display', 'block');
                             showFunction(currFunc);
                             break;
                         }
                     }
                 }
             }
-        ],
-        onRender: function(evt) {
-            this.content('main', grid);
-        }
+        ]
     });
+    function initTrace() {
+        function createDom(data) {
+            var f = functionMap.functions[data.id];
+            var node = document.createElement('div');
+            node.id = 'trace-' + data.id;
+            node.className = 'trace close';
+            var icon = node.appendChild(document.createElement('a'));
+            icon.className = 'icon';
+            icon.onclick = toggle;
+            var file = functionMap.files[f.fileId];
+            var name = node.appendChild(document.createElement('a'));
+            name.appendChild(document.createTextNode(functionMap.functions[data.id].name + '(' + data.args.join(', ') + ')'));
+            if (file) {
+                name.title = file + ':' + f.row + ':' + f.col;
+                name.onclick = showSource;
+            }
+            for (var i = 0; i < data.children.length; i++) {
+                node.appendChild(createDom(data.children[i]));
+            }
+            return node;
+        }
+        function toggle(evt) {
+            var target = $(evt.target.parentNode);
+            if (target.hasClass('open')) {
+                target.removeClass('open');
+                target.addClass('close');
+            } else {
+                target.removeClass('close');
+                target.addClass('open');
+            }
+        }
+
+        $('#content-trace').append(createDom(root));
+    }
+    
+    var root = JSON.parse(localStorage.getItem("jstools.profile"));
+    initProfile();
+    initTrace();
     
     function onResize() {
         var height = $(window).height();
         $('#layout').css('height', height + 'px');
     }
     
-    var summary = {};
     function esc(text) {
         if (!text) return text;
         return text
@@ -69,89 +165,40 @@ $(function() {
             .replace(/>/g, '&gt;')
             .replace(/"/g, '&quot;');
     }
-    function calc(data) {
-        var sum = summary[data.id];
-        if (data.id in summary) {
-            sum.count++;
-            sum.elapsed += data.elapsed;
-        } else {
-            sum = summary[data.id] = {
-                    id: data.id,
-                    fileName: functionMap.files[functionMap.functions[data.id].fileId],
-                    name: functionMap.functions[data.id].name,
-                    count: 1,
-                    elapsed: data.elapsed,
-                    effective: data.elapsed
-                };
-        }
-        for (var i = 0; i < data.children.length; i++) {
-            calc(data.children[i]);
-        }
-        for (var i = 0; i < data.children.length; i++) {
-            sum.effective -= data.children[i].elapsed;
-        }
-    }
-    function show() {
-        var array = [];
-        for (var key in summary) {
-            array.push(summary[key]);
-        }
-        array.sort(function(a, b) {
-            return a.elapsed > b.elapsed ? -1 : (a.elapsed < b.elapsed ? 1 : 0);
-        });
-        for (var i = 0; i < array.length; i++) {
-            var v = array[i];
-            grid.add({
-               recid: v.id,
-               name: esc(v.name),
-               count: v.count,
-               elapsed: v.elapsed,
-               effective: v.effective
-            });
-        }
-    }
     function showFunction(id) {
-        var f = functionMap.functions[id];
-        var fileName = 'sources/' + functionMap.files[f.fileId] + '.html#line-' + f.row;
-        w2ui.layout.load('main', fileName, null, function(evt) {
-            var obj = $('#line-' + f.row).css('background', 'yellow');
+        function hilit(id) {
+            $('pre.line.hilit').removeClass('hilit');
+            var obj = $('#line-' + id).addClass('hilit');
             var offsetParent = obj[0].offsetParent;
             var offsetTop = obj[0].offsetTop;
             offsetParent.scrollTop = offsetTop - offsetParent.offsetHeight / 2;
-            console.log(offsetParent.offsetHeight);
-        });
+        }
+        var f = functionMap.functions[id];
+        var fileName = 'sources/' + functionMap.files[f.fileId] + '.html';
+
+        if ($('#content-source').attr("title") != fileName) {
+            $.ajax({
+                type: "GET",
+                url: fileName,
+                dataType: "html",
+                success: function(html){
+                    $("#content-source").html('');
+                    $("#content-source").append(html);
+                    $("#content-source").attr('title', fileName);
+                    hilit(f.row);
+                }
+            });
+        } else {
+            hilit(f.row);
+        }
         currFunc = id;
     }
     function showSource(evt) {
-        var p = evt.target.title.split(/:/);
-        window.open("sources/" + p[0] + ".html#line-" + p[1], "source");
-    }
-    function hasClass(node, name) {
-        if (!node.className) {
-            return false;
-        } else {
-            return (' ' + node.className + ' ').includes(' ' + name + ' ');
-        }
-    }
-    function removeClass(node, name) {
-        if (hasClass(node, name)) {
-            var parts = node.className.trim().split(/ +/);
-            parts.splice(parts.indexOf(name), 1);
-            node.className = parts.join(' ');
-        }
-    }
-    function addClass(node, name) {
-        if (!hasClass(node, name)) {
-            node.className = node.className ?
-                    node.className + ' ' + name :
-                    name;
-        }
+        currFunc = evt.target.parentNode.id.replace(/^trace\-/, '');
+        w2ui.layout.panels[0].tabs.enable('tab-source');
+        w2ui.layout.panels[0].tabs.click('tab-source');
     }
 
-    var root = JSON.parse(localStorage.getItem("jstools.profile"));
-    calc(root);
-    show();
-    
     onResize();
     $(window).resize(onResize);
 });
